d8c9422b182af2f37e6125b3a80b2df41d22325f
gladiusKatana <garthsnyder@protonmail.com>
2023-03-28 21:29:12 -0400

    update UI (browser dismissals), add view to show updated number of connected peers

--- BatteryStation/MCViewController.swift
+++ BatteryStation/MCViewController.swift
@@ -1,47 +1,57 @@
 import UIKit; import SwiftUI; import MultipeerConnectivity
 
 class MCViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
-    
     var peerID: MCPeerID! /// The peer ID is simply the name of the current device, which is useful for identifying who is joining a session.
     var mcSession: MCSession!
     var mcBrowser: MCBrowserViewController?
     var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
-//    var isHost = true
-    @AppStorage("settingActivated") var isHost_UserDefaultsSetting = Bool()
+    @AppStorage("settingActivated") var isHost_UserDefaultsSetting = true
+    
+    var connectedPeers: Int { mcSession.connectedPeers.count }
+    
+    var statusView = StatusView(showConnectedPeers: .constant(false), connectedPeers: 0)
     
     var batteryStateString = ""
     var batteryState: UIDevice.BatteryState { UIDevice.current.batteryState }
     var batteryLevel: Float { UIDevice.current.batteryLevel }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
+        super.viewDidLoad() //; view.backgroundColor = .red
+        NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange), name: UIDevice.batteryStateDidChangeNotification, object: nil)
         
+        //*
+        //peerID = MCPeerID(displayName: UIDevice.current.name)
+        //mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        //mcSession.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) { print("viewWillAppear ... connected peers: \(mcSession != nil ? "\(connectedPeers)" : "[nil because mcSession is nil]")")
+        super.viewWillAppear(animated)
+        
+        //*
         peerID = MCPeerID(displayName: UIDevice.current.name)
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         mcSession.delegate = self
         
-        if isHost_UserDefaultsSetting {
+        let isHost = isHost_UserDefaultsSetting
+        
+        //DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+        if isHost {
             let hostAction = UIAlertAction(title: "hosting session", style: .default)
-            startHosting(action: hostAction) // stopHosting(action: action)
-        } else {
+            self.startHosting(action: hostAction)
+        }
+        else {
             let joinAction = UIAlertAction(title: "joining session", style: .default)
-            joinSession(action: joinAction)
+            self.joinSession(action: joinAction)
         }
         
-        NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange), name: UIDevice.batteryStateDidChangeNotification, object: nil)
+        //guard let self = self else { return }
         
-        let swiftUIView = SomeSwiftUIView() // swiftUIView is View
-        let childView = UIHostingController(rootView: swiftUIView)
+        setupStatusView()
         
-        childView.view.backgroundColor = .purple
-        
-        addChild(childView)
-        childView.view.frame = self.view.bounds
-        self.view.addSubview(childView.view)
-        childView.didMove(toParent: self)
+        //}
     }
     
     @objc func batteryLevelDidChange(_ notification: Notification) {    //print("\n\nBATTERY LEVEL:\n\(batteryLevel * 100)\n")
@@ -73,7 +83,7 @@ class MCViewController: UIViewController, MCSessionDelegate, MCBrowserViewContro
         } else { print("\nno connected peers\n") }
     }
     
-    func startHosting(action: UIAlertAction!) { print("starting hosting")
+    func startHosting(action: UIAlertAction!) { //print("starting hosting")
         mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-kb", discoveryInfo: nil, session: mcSession) // _hws-kb._tcp
         mcAdvertiserAssistant.start()
     }
@@ -85,42 +95,39 @@ class MCViewController: UIViewController, MCSessionDelegate, MCBrowserViewContro
         present(mcBrowser, animated: true)
     }
     
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    func session(_ session: MCSession, peer peerID: MCPeerID,
+                 didChange state: MCSessionState) {
         switch state {
         case MCSessionState.connected:
-            print("\nConnected: \(peerID.displayName)\n")
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) { [weak self] in
-                self?.mcBrowser?.dismiss(animated: true)
-            }
+            print("\nConnected: \(peerID.displayName) ... connected peers: \(connectedPeers)")
+            hostSetupStatusView()
+            /*DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) { [weak self] in // !!!
+             self?.mcBrowser?.dismiss(animated: true)
+             }*/
         case MCSessionState.connecting: print("\nConnecting: \(peerID.displayName)")
-        case MCSessionState.notConnected: print("\nNot Connected: \(peerID.displayName)")
+        case MCSessionState.notConnected:
+            print("\nNot Connected: \(peerID.displayName)")
+            hostSetupStatusView()
         @unknown default:
             //fatalError()
             print("\nUNKNOWN error in sessiondidChange: \(peerID.displayName)\n")
         }
     }
     
-    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
-    
-    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
-    
-    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    func session(_ session: MCSession,
+                 didReceive data: Data, fromPeer peerID: MCPeerID) {
         let str = String(decoding: data, as: UTF8.self)
-        let alert = UIAlertController(title: "Battery Level Update", message: str, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Battery Level Update", message: str, preferredStyle: .alert)  // [\(connectedPeers) connected peers]
         
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: { _ in // action in
-//            switch action.style {
-//            case .default: print("\n\nalert-tapped-OK--default\n\n")
-//            case .cancel: print("\n\nalert-tapped-OK--cancel\n\n")
-//            case .destructive: print("\n\nalert-tapped-OK--destructive")
-//            @unknown default:
-//                print("\nUNKNOWN error in session didReceive\n")
-//                fatalError()
-//            }
+            /*switch action.style {
+             case .default: print("\n\nalert-tapped-OK--default\n\n")
+             case .cancel: print("\n\nalert-tapped-OK--cancel\n\n")
+             case .destructive: print("\n\nalert-tapped-OK--destructive")
+             @unknown default:
+             print("\nUNKNOWN error in session didReceive\n")
+             fatalError()
+             }*/
         }))
         DispatchQueue.main.async { [weak self] in
             self?.present(alert, animated: true, completion: nil)
@@ -137,20 +144,72 @@ class MCViewController: UIViewController, MCSessionDelegate, MCBrowserViewContro
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
         dismiss(animated: true)
     }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        ////trySendingBatteryData() /// !!!
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        ////trySendingBatteryData() /// !!!
+        dismiss(animated: true)
+    }
+    
+    func hostSetupStatusView() {
+        DispatchQueue.main.async() { [unowned self] in
+            if self.isHost_UserDefaultsSetting {
+                setupStatusView() //refreshRootViewController()
+            }
+        }
+    }
+    
+    func setupStatusView() {
+        statusView = StatusView(showConnectedPeers: self.$isHost_UserDefaultsSetting, connectedPeers: self.connectedPeers)
+        let childView = UIHostingController(rootView: statusView)
+        childView.view.backgroundColor = .purple // should not see this though
+        
+        self.addChild(childView)
+        childView.view.frame = self.view.bounds
+        self.view.addSubview(childView.view)
+        childView.didMove(toParent: self)
+    }
 }
 
-struct SomeSwiftUIView : View {
+struct StatusView : View {
     @AppStorage("settingActivated") var isHost_UserDefaultsSetting = true
+    @Binding var showConnectedPeers : Bool
+    var connectedPeers = -1
     
     var body: some View {
         Form {
             
             Toggle("is host device?", isOn: $isHost_UserDefaultsSetting)
-                .onChange(of: isHost_UserDefaultsSetting) { value in
-                    print("\nupdated isHost setting to \(value)\n")
+                .onChange(of: isHost_UserDefaultsSetting) { value in // print("\nupdated isHost setting to \(value) ... \n")
+                    
+                    ///if !showConnectedPeers {
+                    refreshRootViewController()
+                    ///}
+                    
                 }
                 .tint(.orange)
+            
+            if showConnectedPeers {
+                let s = connectedPeers == 1 ? "" : "s"
+                Text("\(connectedPeers) peer\(s) connected")
+            } else {
+                Text("scanning for peers")
+            }
             
         }
     }
 }
+
+func refreshRootViewController() { print("\nrefreshing view controller")
+    if let apdel = UIApplication.shared.delegate as? AppDelegate,
+       let window = apdel.window {
+        window.rootViewController = UINavigationController(rootViewController: apdel.mcVC)
+    } else {
+        print("\n\nAPP DELEGATE DOWNCAST FAILURE\n\n")
+    }
+}
+
